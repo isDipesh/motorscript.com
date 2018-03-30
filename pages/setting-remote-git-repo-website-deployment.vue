6f90c305df30ce99247656700ea6d69e74b651b6
Dipesh Acharya <xtranophilist@gmail.com>
2018-03-30 18:46:05 +0545

    Reactive "Using git to push websites to server"

--- pages/setting-remote-git-repo-website-deployment.vue
+++ pages/setting-remote-git-repo-website-deployment.vue
@@ -4,18 +4,27 @@
 
         <div class="content" itemprop="articleBody">
 
-            <p>
+            <div class="variables">
+                This cheatsheet will push your repo to the <i class="hl">app</i>
+                directory inside a user home directory in the remote server.
+                <ul>
+                    <li>Remote/Server (domain name or IP): <input v-model="remote"></li>
+                    <li>User on Server : <input v-model="user"></li>
+                    <li>SSH Port : <input v-model="ssh_port"></li>
+                </ul>
+            </div>
+
             <h3><span class="caps">SSH</span> into the server</h3>
 
             <pre>
 <span class="prefix">cd</span>
-<span class="prefix">mkdir repo.git app conf logs media static</span>
+<span class="prefix">mkdir repo.git {{project_dir}} conf logs media static</span>
 <span class="prefix">cd repo.git</span>
 <span class="prefix">git init --bare</span>
 <span class="prefix">git --bare update-server-info</span>
 <span class="prefix">git config core.bare false</span>
 <span class="prefix">git config receive.denycurrentbranch ignore</span>
-<span class="prefix">git config core.worktree /home/user/app/</span>
+<span class="prefix">git config core.worktree /home/{{user}}/{{project_dir}}/</span>
 
 <span class="prefix">cat &gt; hooks/post-receive &lt;&lt;EOF
 #!/bin/sh
@@ -30,15 +39,14 @@ EOF</span>
             <h3>On the client</h3>
 
             <pre>
-<span class="prefix">git remote add server user@awecode.com:/home/user/repo.git/</span>
-<span class="prefix">ssh-copy-id user@awecode.com</span>
+<span class="prefix" v-if="ssh_port=='22'">git remote add server {{user}}@{{remote}}:/home/{{user}}/repo.git/</span><span class="prefix" v-else>git remote add server ssh://{{user}}@{{remote}}:{{ssh_port}}/home/{{user}}/repo.git/</span>
+<span class="prefix" v-if="ssh_port=='22'">ssh-copy-id {{user}}@{{remote}}</span><span class="prefix" v-else>ssh-copy-id {{user}}@{{remote}} -p {{ssh_port}}</span>
 <span class="prefix">git push server --all</span>
 </pre>
-
-            If the server uses port other than 22, use the following on client to add git remote:
-            <pre>
-<span class="prefix">git remote add server ssh://user@awecode:port_no/home/user/repo.git/</span>
-        </pre>
+            
+            <p>
+                You may want to modify your <i class="hl">git post-receive</i> hook to run custom commands on the server, like running database migrations, notifying services of new deployments, pushing static files to CDN, etc. 
+            </p>
 
         </div>
 
@@ -51,5 +59,13 @@ EOF</span>
   export default {
     mixins: [BlogTitle],
     components: {BlogTitle},
+    data() {
+      return {
+        project_dir: 'app',
+        user: 'user',
+        remote: 'awecode.com',
+        ssh_port: '22'
+      }
+    }
   }
 </script>
