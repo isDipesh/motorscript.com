903925f8480fcb4b19b268e78f424c4aba511d98
Dipesh Acharya <xtranophilist@gmail.com>
2018-03-30 19:43:37 +0545

    Create empty blog for bootstrapping

---
+++ pages/empty.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <BlogTitle title="" published=""/>
+
+        <div class="content" itemprop="articleBody">
+        </div>
+    </div>
+</template>
+
+<script>
+  import BlogTitle from '../components/BlogTitle.vue';
+
+  export default {
+    mixins: [BlogTitle],
+    components: {BlogTitle},
+  }
+</script>
