0a66c01cd112ae9aeabf8b68bd23e140730be2dd
Dipesh Acharya <xtranophilist@gmail.com>
2018-04-04 14:37:13 +0545

    mf

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
     <ul class="posts">
-        <li><router-link to="/django-deployment-cheatsheet/">Simple Django Deployment Cheat-sheet</router-link></li>
-        <li><router-link to="/selectize-js-binding-knockout-js/">Selectize.js binding for Knockout.js</router-link></li>
-        <li><router-link to="/setting-remote-git-repo-website-deployment/">Using git to deploy to server</router-link></li>
+        <li><router-link to="django-deployment-cheatsheet/">Simple Django Deployment Cheat-sheet</router-link></li>
+        <li><router-link to="selectize-js-binding-knockout-js/">Selectize.js binding for Knockout.js</router-link></li>
+        <li><router-link to="setting-remote-git-repo-website-deployment/">Using git to deploy to server</router-link></li>
         <li>
-            <router-link to="/hiding-header-and-footer-only-in-certain-pages-with-libreoffice-writer/">
+            <router-link to="hiding-header-and-footer-only-in-certain-pages-with-libreoffice-writer/">
                 Hiding header and footer only in certain pages with LibreOffice Writer
             </router-link>
         </li>
-        <li><router-link to="/django-froala-editor/">Django package for Froala WYSIWYG Editor</router-link></li>
-        <li><router-link to="/dbf2sql/">dbf2sql – dbf to sql converter</router-link></li>
-        <li><router-link to="/what-happened-to-ipv5/">What happened to IPv5?</router-link></li>
-        <li><router-link to="/gnome-shell-extension-sensors/">Gnome Shell Extension : Sensors</router-link></li>
+        <li><router-link to="django-froala-editor/">Django package for Froala WYSIWYG Editor</router-link></li>
+        <li><router-link to="dbf2sql/">dbf2sql – dbf to sql converter</router-link></li>
+        <li><router-link to="what-happened-to-ipv5/">What happened to IPv5?</router-link></li>
+        <li><router-link to="gnome-shell-extension-sensors/">Gnome Shell Extension : Sensors</router-link></li>
     </ul>
 </template>
 
