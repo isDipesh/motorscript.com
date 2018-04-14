e04e43dd8441f472a9a2afb2d668944aa20e0fea
Dipesh Acharya <xtranophilist@gmail.com>
2018-04-14 19:44:43 +0545

    Person schema in footer

--- layouts/BlogFooter.vue
+++ layouts/BlogFooter.vue
@@ -1,11 +1,16 @@
 <template>
     <footer>
         <div class="center">
-            Dipesh Acharya's weblog of cheat-sheets
+            <span itemprop="author" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Dipesh Acharya</span>'s weblog of cheat-sheets
+          <meta itemprop="jobTitle" content="COO">
+          <meta itemprop="worksFor" content="Awecode">
+        </span>
         </div>
         <div class="center">
             Powered by <a target="_blank" href="https://nuxtjs.org" rel="noreferrer noopener">Nuxt.js</a> |
-            Hosted on <a target="_blank" href="https://github.com/xtranophilist/motorscript.com" rel="noreferrer noopener">Github</a>
+            Hosted on <a target="_blank" href="https://github.com/xtranophilist/motorscript.com"
+                         rel="noreferrer noopener">Github</a>
             | Built using <a target="_blank" href="https://travis-ci.org" rel="noreferrer noopener">Travis CI</a>
         </div>
     </footer>
