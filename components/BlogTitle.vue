01eef0887326f446cfd73179e34ce1597069d5a9
Dipesh Acharya <xtranophilist@gmail.com>
2018-04-14 19:42:28 +0545

    Fix dateModified schema prop

--- components/BlogTitle.vue
+++ components/BlogTitle.vue
@@ -2,7 +2,7 @@
     <div>
         <h1 class="title" itemprop="name headline">{{title}}</h1>
         <div class="time">Published: <time itemprop="datePublished">{{published}}</time></div>
-        <div v-if="updated" class="time">Updated: <time itemprop="datePublished">{{updated}}</time></div>
+        <div v-if="updated" class="time">Updated: <time itemprop="dateModified">{{updated}}</time></div>
     </div>
 </template>
 
