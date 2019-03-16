7498d0d0fb94f56580c8a96272d9506eac26ee9f
Dipesh Acharya <xtranophilist@gmail.com>
2019-03-17 01:50:10 +0545

    Enabel G A

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,13 +25,13 @@ module.exports = {
   css: ["assets/main.scss"],
   modules: [
     "@nuxtjs/sitemap",
-    // [
-      // "@nuxtjs/google-adsense",
-      // {
-      //   id: "ca-pub-8986893739514632",
-      //   pageLevelAds: true,
-      // }
-    // ]
+    [
+      "@nuxtjs/google-adsense",
+      {
+        id: "ca-pub-8986893739514632",
+        pageLevelAds: true,
+      }
+    ]
   ],
   plugins: [
     {src: '~/plugins/prism.js', ssr: false},
