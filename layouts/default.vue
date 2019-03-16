184b8b4cf75038929f867620c39390aca6980096
Dipesh Acharya <xtranophilist@gmail.com>
2019-03-16 22:09:08 +0545

    Use nuxt-module for adsense

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,34 @@
 <template>
-    <div class="container">
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-8986893739514632",
-        enable_page_level_ads: true
-      });
-    </script>
-        <BlogHeader/>
-        <main itemscope itemtype="https://schema.org/Blog">
-            <nuxt/>
-            <div id="disqus_thread" itemscope itemtype="https://schema.org/UserComments"></div>
-            <script>
-              var disqus_config = function () {
-              };
-              (function () { // DON'T EDIT BELOW THIS LINE
-                var d = document, s = d.createElement('script');
-                s.src = 'https://motorscript.disqus.com/embed.js';
-                s.setAttribute('data-timestamp', +new Date());
-                (d.head || d.body).appendChild(s);
-              })();
-            </script>
-        </main>
-        <BlogFooter/>
-    </div>
+  <div class="container">
+    <BlogHeader/>
+    <main itemscope itemtype="https://schema.org/Blog">
+      <nuxt/>
+      <div id="disqus_thread" itemscope itemtype="https://schema.org/UserComments"></div>
+      <script>
+        var disqus_config = function() {};
+        (function() {
+          // DON'T EDIT BELOW THIS LINE
+          var d = document,
+            s = d.createElement("script");
+          s.src = "https://motorscript.disqus.com/embed.js";
+          s.setAttribute("data-timestamp", +new Date());
+          (d.head || d.body).appendChild(s);
+        })();
+      </script>
+    </main>
+    <BlogFooter/>
+  </div>
 </template>
 
 
 <script>
-  import BlogHeader from "./BlogHeader.vue"
-  import BlogFooter from "./BlogFooter.vue"
+import BlogHeader from "./BlogHeader.vue";
+import BlogFooter from "./BlogFooter.vue";
 
-  export default {
-    components: {
-      BlogHeader, BlogFooter
-    },
+export default {
+  components: {
+    BlogHeader,
+    BlogFooter
   }
+};
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,59 @@
 module.exports = {
   head: {
-    titleTemplate: '%s - motorscript',
+    titleTemplate: "%s - motorscript",
     htmlAttrs: {
-      lang: 'en',
+      lang: "en"
     },
     meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {name: 'author', content: 'Dipesh Acharya'},
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "author", content: "Dipesh Acharya" }
     ],
-    link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
-    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     bodyAttrs: {
-      itemscope: '',
-      itemtype: 'https://schema.org/WebPage'
+      itemscope: "",
+      itemtype: "https://schema.org/WebPage"
     }
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: {color: '#000'},
+   ** Customize the progress bar color
+   */
+  loading: { color: "#000" },
   /*
-  ** Build configuration
-  */
-  css: [
-    'assets/main.scss'
-  ],
+   ** Build configuration
+   */
+  css: ["assets/main.scss"],
   modules: [
-    '@nuxtjs/sitemap'
+    "@nuxtjs/sitemap",
+    [
+      "@nuxtjs/google-adsense",
+      {
+        id: "ca-pub-8986893739514632",
+        pageLevelAds: true,
+      }
+    ]
   ],
   generate: {
-    routes: ['404']
+    routes: ["404"]
   },
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend(config, {isDev, isClient}) {
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   },
   sitemap: {
-    path: '/sitemap.xml',
-    hostname: 'https://motorscript.com',
-    generate: true,
+    path: "/sitemap.xml",
+    hostname: "https://motorscript.com",
+    generate: true
   }
 };
