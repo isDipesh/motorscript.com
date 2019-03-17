e20766852523764473f95c1191e53dba8fcaaf17
Dipesh Acharya <xtranophilist@gmail.com>
2019-03-17 19:44:14 +0545

    Add "Install Preeti Font in Linux (Fedora)"

--- pages/archive.vue
+++ pages/archive.vue
@@ -10,6 +10,12 @@
       </li>
 
       <li>
+        <router-link to="install-preeti-font-linux">
+          Install Preeti Font in Linux (Fedora)
+        </router-link>
+      </li>
+
+      <li>
         <router-link to="crop-to-selection-keyboard-shortcut-gimp">
           'Crop to Selection' keyboard shortcut for GIMP
         </router-link>
