e8128a90731907cd4b696f266018b1a0ca08c26d
Dipesh Acharya <xtranophilist@gmail.com>
2021-02-10 22:10:50 +0545

    Disable prism

--- plugins/prism.js
+++ plugins/prism.js
@@ -1,17 +1,17 @@
 import "prism-es6/themes/prism.css";
-import Vue from "vue";
+// import Vue from "vue";
 
-import Prism from "prism-es6";
+// import Prism from "prism-es6";
 
-require("prismjs/components/prism-markup.min.js");
-require("prismjs/components/prism-javascript.min.js");
-require("prismjs/components/prism-bash.min.js");
-require("prismjs/components/prism-python.min.js");
+// require("prismjs/components/prism-markup.min.js");
+// require("prismjs/components/prism-javascript.min.js");
+// require("prismjs/components/prism-bash.min.js");
+// require("prismjs/components/prism-python.min.js");
 
-require("prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js");
+// require("prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js");
 
-const directive = function(el) {
-  Prism.highlightAllUnder(el);
-};
+// const directive = function(el) {
+//   Prism.highlightAllUnder(el);
+// };
 
-Vue.directive("highlight", directive);
+// Vue.directive("highlight", directive);
