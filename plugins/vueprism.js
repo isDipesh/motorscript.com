2571b0ced02fc12ef1c4179e732a9d08c395e5d5
Dipesh Acharya <xtranophilist@gmail.com>
2021-02-21 21:40:03 +0545

    Refactor

--- plugins/vueprism.js
+++ plugins/vueprism.js
@@ -16,20 +16,25 @@ require("prismjs/components/prism-nginx.min.js");
 
 const directive = function(rootEl, directiveObj, node) {
   node.context.$nextTick(() => {
-    let preTags = rootEl.getElementsByTagName("pre");
-    const clonePres = document.querySelectorAll("pre.clone");
-    for (var i = 0; i < clonePres.length; ++i) {
-      clonePres[i].remove();
+    // Remove all clones
+    const clonePreTags = document.querySelectorAll("pre.clone");
+    for (var i = 0; i < clonePreTags.length; ++i) {
+      clonePreTags[i].remove();
     }
-    preTags = rootEl.getElementsByTagName("pre");
-    for (let el of preTags) {
+
+    // Find all pre tags
+    for (let el of rootEl.getElementsByTagName("pre")) {
+      // Make sure the tag isn't a clone
       if (!el.classList.contains("clone")) {
+        // Clone the node and hide the original
         const clonedNode = el.cloneNode(true);
-        clonedNode.style["display"] = "block";
         el.classList.add("cloned");
         el.style["display"] = "none";
+        clonedNode.style["display"] = "block";
         clonedNode.classList.add("clone");
+        // Hightlight the cloned node
         Prism.highlightAllUnder(clonedNode);
+        // Add cloned node to DOM
         el.parentNode.insertBefore(clonedNode, el.nextSibling);
       }
     }
