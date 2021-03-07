5c9bd37ccebeca682255c87e636e3dbde9743679
Dipesh Acharya <xtranophilist@gmail.com>
2021-03-07 20:31:35 +0545

    Enable disqus

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="container">
-    <BlogHeader/>
+    <BlogHeader />
     <main itemscope itemtype="https://schema.org/Blog">
-      <nuxt/>
-      <div id="disqus_thread" itemscope itemtype="https://schema.org/UserComments"></div>
-      <!-- <no-ssr>
-      <script>
-        var disqus_config = function() {};
-        (function() {
-          // DON'T EDIT BELOW THIS LINE
-          var d = document,
-            s = d.createElement("script");
-          s.src = "https://motorscript.disqus.com/embed.js";
-          s.setAttribute("data-timestamp", +new Date());
-          (d.head || d.body).appendChild(s);
-        })();
-      </script>
-      </no-ssr> -->
+      <nuxt />
+      <div
+        id="disqus_thread"
+        itemscope
+        itemtype="https://schema.org/UserComments"
+      ></div>
+      <client-only>
+        <script>
+          var disqus_config = function() {};
+          (function() {
+            // DON'T EDIT BELOW THIS LINE
+            var d = document,
+              s = d.createElement("script");
+            s.src = "https://motorscript.disqus.com/embed.js";
+            s.setAttribute("data-timestamp", +new Date());
+            (d.head || d.body).appendChild(s);
+          })();
+        </script>
+      </client-only>
     </main>
-    <BlogFooter/>
+    <BlogFooter />
   </div>
 </template>
-
 
 <script>
 import BlogHeader from "./BlogHeader.vue";
@@ -31,9 +34,6 @@ export default {
   components: {
     BlogHeader,
     BlogFooter
-  },
-  // mounted(){
-    // Prism.highlightAll();
-  // }
+  }
 };
 </script>
